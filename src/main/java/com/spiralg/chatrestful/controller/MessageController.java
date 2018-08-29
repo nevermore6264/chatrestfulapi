ee17493df84beb9af877096aa01c2ec7aaa6df05
Thanh Dat Nguyen <datnt@nal.vn>
2018-08-29 15:01:26 +0700

    junit test get messages

--- src/main/java/com/spiralg/chatrestful/controller/MessageController.java
+++ src/main/java/com/spiralg/chatrestful/controller/MessageController.java
@@ -1,6 +1,5 @@
 package com.spiralg.chatrestful.controller;
 
-import com.google.gson.Gson;
 import com.spiralg.chatrestful.model.Message;
 import com.spiralg.chatrestful.model.User;
 import com.spiralg.chatrestful.service.MessageService;
@@ -16,48 +15,65 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-@RequestMapping("/api/message")
+@RequestMapping("/api/messages")
 public class MessageController {
 
     private MessageService messageService;
     private UserService userService;
-//    private Gson gson = new Gson();
     private static String pattern = "\\d{4}-\\d{1,2}-\\d{1,2}";
 
     @Autowired
-    public MessageController(MessageService messageService, UserService userService){
+    public MessageController(MessageService messageService, UserService userService) {
         this.messageService = messageService;
         this.userService = userService;
     }
 
     @GetMapping
-    public ResponseEntity<List<Message>> getAll(@RequestParam(value = "sender", defaultValue = "", required = false) String sender, @RequestParam(value = "receiver", defaultValue = "", required = false) String receiver, @RequestParam(value = "date", defaultValue = "", required = false) String create_date){
-        User senderUser = userService.findByName(sender);
-        User receiverUser = userService.findByName(receiver);
+    public ResponseEntity<Object> getAll(@RequestParam(value = "sender", defaultValue = "", required = false) String sender, @RequestParam(value = "receiver", defaultValue = "", required = false) String receiver, @RequestParam(value = "date", defaultValue = "", required = false) String create_date) {
+        User senderUser = userService.findByName( sender );
+        User receiverUser = userService.findByName( receiver );
 
-        if (sender.trim().equals("") || receiver.trim().equals("") || senderUser != null || receiverUser != null){
-            if (create_date.matches(pattern)){
-                List<Message> messages = messageService.getAll(sender, receiver,create_date);
-                return new ResponseEntity<>( messages, HttpStatus.OK);
-            }else {
+        if (sender.trim().equals( "" ) || receiver.trim().equals( "" ) || senderUser != null || receiverUser != null) {
+            if (create_date.matches( pattern ) || sender.trim().equals( "" ) || receiver.trim().equals( "" ) || create_date == null) {
+                List<Message> messages = messageService.getAll( sender, receiver, create_date );
+                return new ResponseEntity<>( new getMessage( messages.size(), messages ), HttpStatus.OK );
+            } else {
                 return new ResponseEntity<>( HttpStatus.BAD_REQUEST );
             }
-        }else{
-            if (senderUser == null || receiverUser == null){
+        } else {
+            if (senderUser == null || receiverUser == null ) {
                 return new ResponseEntity<>( HttpStatus.NOT_FOUND );
-            }else{
+            } else {
                 List<Message> messages = messageService.getAll( sender, receiver, create_date );
-                return new ResponseEntity<>( messages, HttpStatus.OK );
+                return new ResponseEntity<>( new getMessage( messages.size(), messages ), HttpStatus.OK );
             }
         }
     }
 
     @GetMapping(value = {"/date"})
-    public ResponseEntity<List<Message>> getAllByDate(@RequestParam("date") String create_date){
-        if (create_date.matches( pattern )){
+    public ResponseEntity<Object> getAllByDate(@RequestParam("date") String create_date) {
+        if (create_date.matches( pattern )) {
             List<Message> messages = messageService.getAllByDay( create_date );
-            return new ResponseEntity<>( messages, HttpStatus.OK );
+            return new ResponseEntity<>( new getMessage( messages.size(), messages ), HttpStatus.OK );
         }
         return new ResponseEntity<>( HttpStatus.BAD_REQUEST );
+    }
+
+    class getMessage {
+        public getMessage(int total, List<Message> items) {
+            this.total = total;
+            this.items = items;
+        }
+
+        public int getTotal() {
+            return total;
+        }
+
+        public List<Message> getItems() {
+            return items;
+        }
+
+        private int total;
+        private List<Message> items;
     }
 }
---
+++ src/test/java/com/spiralg/chatrestful/controller/TestMessage.java
@@ -0,0 +1,208 @@
+package com.spiralg.chatrestful.controller;
+
+import com.spiralg.chatrestful.model.Message;
+import com.spiralg.chatrestful.model.User;
+import com.spiralg.chatrestful.service.MessageService;
+import com.spiralg.chatrestful.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TestMessage {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private MessageService messageService;
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private MessageController messageController;
+
+    @Before
+    public void init() throws Exception {
+        MockitoAnnotations.initMocks( this );
+        mockMvc = MockMvcBuilders
+                .standaloneSetup( messageController )
+                .build();
+    }
+
+    @Test
+    public void getMessageByCorrectDateHaveResult() throws Exception {
+        List<Message> messages = new ArrayList<>();
+        messages.add( new Message( 1, "hello", "2018-08-08", "datnt", "khoand" ) );
+
+        given( userService.findByName( "datnt" ) ).willReturn( new User( 1, "datnt", (byte) 0 ) );
+        given( userService.findByName( "khoand" ) ).willReturn( new User( 2, "khoand", (byte) 0 ) );
+        given( messageService.getAll( "datnt", "khoand", "2018-08-08" ) ).willReturn( messages );
+
+        mockMvc.perform( get( "/api/messages" ).param( "sender", "datnt" ).param( "receiver", "khoand" ).param( "date", "2018-08-08" )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 ) )
+                .andExpect( status().isOk() )
+                .andExpect( jsonPath( "$.total", is( Integer.valueOf( 1 ) ) ) )
+                .andExpect( jsonPath( "$.items[0].sender", is( "datnt" ) ) )
+                .andExpect( jsonPath( "$.items[0].receiver", is( "khoand" ) ) )
+                .andExpect( jsonPath( "$.items[0].message", is( "hello" ) ) )
+                .andExpect( jsonPath( "$.items[0].id", is( 1 ) ) );
+    }
+
+    @Test
+    public void getMessageByCorrectDateHaveNoResult() throws Exception {
+        List<Message> messages = new ArrayList<>();
+
+        given( messageService.getAllByDay( "2018-09-08" ) ).willReturn( messages );
+
+        mockMvc.perform( get( "/api/messages/date" ).param( "date", "2018-09-08" )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 ) )
+                .andExpect( status().isOk() )
+                .andExpect( jsonPath( "$.total", is( Integer.valueOf( 0 ) ) ) );
+    }
+
+    @Test
+    public void getMessageWithOnlyDate() throws Exception {
+        List<Message> messages = new ArrayList<>();
+        messages.add( new Message( 1, "hello", "2018-08-08", "datnt", "khoand" ) );
+
+        given( messageService.getAllByDay( "2018-08-08" ) ).willReturn( messages );
+
+        mockMvc.perform( get( "/api/messages/date" ).param( "date", "2018-08-08" )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 ) )
+                .andExpect( status().isOk() )
+                .andExpect( jsonPath( "$.total", is( Integer.valueOf( 1 ) ) ) )
+                .andExpect( jsonPath( "$.items[0].sender", is( "datnt" ) ) )
+                .andExpect( jsonPath( "$.items[0].receiver", is( "khoand" ) ) )
+                .andExpect( jsonPath( "$.items[0].message", is( "hello" ) ) )
+                .andExpect( jsonPath( "$.items[0].id", is( 1 ) ) );
+    }
+
+    @Test
+    public void getMessageWithOnlySender() throws Exception {
+        List<Message> messages = new ArrayList<>();
+
+        messages.add( new Message( 1, "hello", "2018-08-08", "datnt", "khoand" ) );
+
+        given( userService.findByName( "datnt" ) ).willReturn( new User( 1, "datnt", (byte) 0 ) );
+        given( userService.findByName( "" ) ).willReturn( new User( 2, "khoand", (byte) 0 ) );
+        given( messageService.getAll( "datnt", "", "" ) ).willReturn( messages );
+
+        mockMvc.perform( get( "/api/messages/" ).param( "sender", "datnt" ).param( "receiver", "" ).param( "date", "" )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 ) )
+                .andExpect( status().isOk() )
+                .andExpect( jsonPath( "$.total", is( Integer.valueOf( 1 ) ) ) )
+                .andExpect( jsonPath( "$.items[0].sender", is( "datnt" ) ) )
+                .andExpect( jsonPath( "$.items[0].receiver", is( "khoand" ) ) )
+                .andExpect( jsonPath( "$.items[0].message", is( "hello" ) ) )
+                .andExpect( jsonPath( "$.items[0].id", is( 1 ) ) );
+    }
+
+    @Test
+    public void getMessageWithOnlyReceiver() throws Exception {
+        List<Message> messages = new ArrayList<>();
+
+        given( userService.findByName( "" ) ).willReturn( new User( 1, "datnt", (byte) 0 ) );
+        given( userService.findByName( "khoand" ) ).willReturn( new User( 2, "khoand", (byte) 0 ) );
+
+        mockMvc.perform( get( "/api/messages/" ).param( "sender", "" ).param( "receiver", "khoand" ).param( "date", "" )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 ) )
+                .andExpect( status().isOk() )
+                .andExpect( jsonPath( "$.total", is( Integer.valueOf( 0 ) ) ) );
+    }
+
+    @Test
+    public void getMessageWithWrongFomatDate() throws Exception {
+        List<Message> messages = new ArrayList<>();
+
+        given( messageService.getAllByDay( "08-08-2018" ) ).willReturn( messages );
+
+        mockMvc.perform( get( "/api/messages/date" ).param( "date", "08-08-2018" )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 ) )
+                .andExpect( status().isBadRequest() );
+    }
+
+    @Test
+    public void getMessageWithWrongSender() throws Exception {
+        List<Message> messages = new ArrayList<>();
+
+        given( userService.findByName( "khoand" ) ).willReturn( new User( 2, "khoand", (byte) 0 ) );
+        given( messageService.getAll( "hieutt", "khoand", "2018-08-08" ) ).willReturn( messages );
+
+        mockMvc.perform( get( "/api/messages" ).param( "sender", "hieutt" ).param( "receiver", "khoand" ).param( "date", "2018-08-08" )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 ) )
+                .andExpect( status().isOk() )
+                .andExpect( jsonPath( "$.total", is( Integer.valueOf( 0 ) ) ) );
+    }
+
+    @Test
+    public void getMessageWithWrongReceiver() throws Exception {
+        List<Message> messages = new ArrayList<>();
+
+        given( userService.findByName( "datnt" ) ).willReturn( new User( 1, "datnt", (byte) 0 ) );
+        given( messageService.getAll( "datnt", "hieutt", "2018-08-08" ) ).willReturn( messages );
+
+        mockMvc.perform( get( "/api/messages" ).param( "sender", "datnt" ).param( "receiver", "hieutt" ).param( "date", "2018-08-08" )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 ) )
+                .andExpect(status().isOk())
+                .andExpect( jsonPath( "$.total", is( Integer.valueOf( 0 ) ) ) );
+    }
+
+    @Test
+    public void getMessageWithSenderIsWhiteSpace() throws Exception {
+        List<Message> messages = new ArrayList<>();
+        messages.add( new Message( 1, "hello", "2018-08-08", "datnt", "khoand" ) );
+
+        given( messageService.getAll( "", "khoand", "2018-08-08" ) ).willReturn( messages );
+
+        mockMvc.perform( get( "/api/messages" ).param( "sender", " " ).param( "receiver", "khoand" ).param( "date", "2018-08-08" )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 ) )
+                .andExpect( status().isOk() )
+                .andExpect( jsonPath( "$.total", is( Integer.valueOf( 0 ) ) ) );
+    }
+    @Test
+    public void getMessageWithReceiverIsWhiteSpace() throws Exception{
+        List<Message> messages = new ArrayList<>();
+
+        given( userService.findByName( "datnt" ) ).willReturn( new User( 1, "datnt", (byte) 0 ) );
+        given( userService.findByName( "khoand" ) ).willReturn( new User( 2, "khoand", (byte) 0 ) );
+        given( messageService.getAll( "datnt", "", "2018-08-08" ) ).willReturn( messages );
+
+        mockMvc.perform( get( "/api/messages" ).param( "sender", "datnt" ).param( "receiver", " " ).param( "date", "2018-08-08" )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 ) )
+                .andExpect( status().isOk() )
+                .andExpect( jsonPath( "$.total", is( Integer.valueOf( 0 ) ) ) );
+    }
+
+    @Test
+    public void getMessageWithDateIsWhiteSpace() throws Exception{
+        List<Message> messages = new ArrayList<>();
+
+        given( userService.findByName( "datnt" ) ).willReturn( new User( 1, "datnt", (byte) 0 ) );
+        given( userService.findByName( "khoand" ) ).willReturn( new User( 2, "khoand", (byte) 0 ) );
+        given( messageService.getAll( "datnt", "khoand", "" ) ).willReturn( messages );
+
+        mockMvc.perform( get( "/api/messages" ).param( "datnt", "datnt" ).param( "receiver", "khoand" ).param( "date", " " )
+                .contentType( MediaType.APPLICATION_JSON_UTF8 ) )
+                .andExpect( status().isOk() )
+                .andExpect( jsonPath( "$.total", is( Integer.valueOf( 0 ) ) ) );
+    }
+}
